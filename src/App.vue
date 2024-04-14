6f960dc9b6343e6869fe9694d191d50063597d81
Arthur Escalera <arthurescalera@gmail.com>
2024-04-14 17:23:59 -0300

    First change

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,6 @@
 
 <template>
   <div>
-    <h1 class="text-3xl">Hello World</h1>
-    <h1 class="text-3xl font-bold underline">Hello World</h1>
-    <h1 class="text-3xl font-bold underline">Hello World</h1>
-    <h1 class="text-3xl font-bold underline">Hello World</h1>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
       <Icon />
   </div>
 </template>
